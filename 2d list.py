15e3418465e8fe1fa8f9cfa2726bc341b6b41402
minion779 <157313373+minion779@users.noreply.github.com>
2025-02-27 19:06:21 +0000

    Add files via upload

---
+++ 2d list.py
@@ -0,0 +1,11 @@
+# 2D  list - Rows and  Columns
+
+num = [[0,1,2],
+        [4,6,7],
+        [7,8,9]]
+
+print(num[1][1])
+
+for i in range(len(num)):
+    for g in range(len(num[i])):
+        print(num[i][g])
