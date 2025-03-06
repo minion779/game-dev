2fb323bd43e85b71653f1cb22de63cfe936f2058
minion779 <157313373+minion779@users.noreply.github.com>
2025-03-06 18:36:11 +0000

    Add files via upload

--- 2D addition.py
+++ 2D addition.py
@@ -1,11 +1,43 @@
+#addidtion
 l1 = [[1,2],[3,4]]
 l2 = [[5,6],[7,8]]
 ResAdd = [[0,0],[0,0]]
 
+
 for i in range(2):
     for g in range(2):
-        ResAdd = l1[i][g]+l2[i][g]
+        ResAdd[i][g] = l1[i][g]+l2[i][g]
     
 for i in range(2):
     for g in range(2):
-        print(ResAdd[i][g], end = " ")
+        print(ResAdd[i][g], end = "*")
+
+#minus
+
+l3 = [[1,2],[3,4]]
+l4 = [[5,6],[7,8]]
+ResMinus = [[0,0],[0,0]]
+
+for i in range(2):
+    for g in range(2):
+        ResMinus[i][g] = l3[i][g]-l4[i][g]
+    
+for i in range(2):
+    for g in range(2):
+        print(ResMinus[i][g], end = "*")
+
+#print diagonals
+
+list5 = []
+print("What dimension do you want?")
+question = int(input(">"))
+
+for i in range(question):
+    temp = []
+    for g in range(question):
+        x = int(input("enter your element"))
+        temp.append(x)
+    list5.append(temp)
+for i in range(question):
+    print(list5[i][i])
+
