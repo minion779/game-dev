c8967e0fc1d33013acf35bdf6701549f9c65fc4f
minion779 <157313373+minion779@users.noreply.github.com>
2025-04-03 19:12:35 +0100

    Add files via upload

--- satconnect.py
+++ satconnect.py
@@ -1,4 +1,5 @@
 import pgzrun
+from time import time
 from random import randint
 
 WIDTH = 800
@@ -21,3 +22,29 @@ def create_satelites():
         satelites.pos = (randint(40, WIDTH-40)), (randint(40, HEIGHT-40))
         satelites.append(satelite)
     start_time = time()
+
+def draw():
+    global total_time
+    screen.blit("space.png", (0,0))
+    numbers = 1
+    for satelite in satelites:
+        screen.draw.text(str(numbers), (satelite.pos[0]+50, satelite.pos[1]+30), fontsize=34, color="white")
+        satelite.draw()
+        numbers += 1
+    for line in lines: 
+        screen.draw.line(line[0], line[1], ("yellow"))
+    
+    if Next_Satelite < num_satelites:
+        total_time = time() - start_time
+        screen.draw.text(str(round(total_time,1)),(10,10),fontsize=30,color="white")
+    else:
+        screen.blit("leaves.jpg",(0,0))
+        screen.draw.text("Good Job",(50,150),fontsize=40,color="black")
+        screen.draw.text(str(round(total_time,1)),(100,300),fontsize=30,color="black")
+
+def update():
+    pass
+
+create_satelites()
+
+pgzrun.go()
